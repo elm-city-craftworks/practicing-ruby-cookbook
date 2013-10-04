e4812f1770afb15b95e8758db632597896bebbe0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-04 14:29:56 +0200

    Create deploy user first

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,15 @@
 # Cookbook Name:: practicingruby
 # Recipe:: default
 #
+# Sets up environment for Practicing Ruby Rails app
+#
 
+# Update Debian package index
 include_recipe "apt::default"
+
+# Include all the pieces
+include_recipe "practicingruby::deploy_user"
 include_recipe "practicingruby::ruby"
 include_recipe "practicingruby::postgresql"
 include_recipe "practicingruby::god"
-include_recipe "practicingruby::deploy_user"
 include_recipe "practicingruby::app"

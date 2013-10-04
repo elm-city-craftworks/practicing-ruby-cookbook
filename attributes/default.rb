41b1ae0b8590d4421b19001add504a8ae606f4f1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-04 10:54:00 +0200

    Re-add node["practicingruby"]["ruby"]["version"]

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,5 @@
 # Attributes:: default
 #
 
-# Override default chruby attributes
-set["chruby"]["rubies"] = {
-  "1.9.3-p392" => false,
-  "2.0.0-p247" => true
-}
-set["chruby"]["default"] = "2.0.0-p247"
+# Ruby version to install
+default["practicingruby"]["ruby"]["version"] = "2.0.0-p247"
--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -5,6 +5,13 @@
 # Installs Ruby and Bundler
 #
 
+# Override default chruby attributes
+node.set["chruby"]["rubies"] = {
+  "1.9.3-p392" => false,
+  node["practicingruby"]["ruby"]["version"] => true
+}
+node.set["chruby"]["default"] = node["practicingruby"]["ruby"]["version"]
+
 # Build and install Ruby versions using chruby und ruby-build
 include_recipe "chruby::system"
 

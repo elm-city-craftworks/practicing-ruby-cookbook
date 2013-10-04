b73862c3cef207da35c26b9cd42877f74d9feef3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-04 15:50:09 +0200

    Create config files for Rails app

--- recipes/app.rb
+++ recipes/app.rb
@@ -12,11 +12,28 @@ package "python-pygments"
 # Create deploy user first
 include_recipe "practicingruby::deploy_user"
 
-# Create config directory
+# Create config directory for Rails app
 config_dir = "/home/#{node["practicingruby"]["deploy"]["username"]}/shared"
 directory config_dir do
   owner  node["practicingruby"]["deploy"]["username"]
   group  node["practicingruby"]["deploy"]["username"]
   mode   "2775"
   action :create
+end
+
+# Create config files for Rails app
+%w(cache_cooker_settings.rb
+   database.yml
+   mailchimp_settings.rb
+   mixpanel.rb
+   omniauth.rb
+   secret_token.rb
+   stripe.rb
+).each do |file|
+  template File.join(config_dir, file) do
+    owner  node["practicingruby"]["deploy"]["username"]
+    group  node["practicingruby"]["deploy"]["username"]
+    mode   "0644"
+    action :create
+  end
 end

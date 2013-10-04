6a5dc9d6dcdf734c66cca0330aa328dba2e915e6
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-04 15:26:24 +0200

    Create config dir for Rails app

--- recipes/app.rb
+++ recipes/app.rb
@@ -3,7 +3,20 @@
 # Recipe:: app
 #
 
-# JavaScript runtime for Rails app
+# Install JavaScript runtime for Rails app
 package "nodejs"
-# For syntax highlighting
+
+# Install Pygments for syntax highlighting
 package "python-pygments"
+
+# Create deploy user first
+include_recipe "practicingruby::deploy_user"
+
+# Create config directory
+config_dir = "/home/#{node["practicingruby"]["deploy"]["username"]}/shared"
+directory config_dir do
+  owner  node["practicingruby"]["deploy"]["username"]
+  group  node["practicingruby"]["deploy"]["username"]
+  mode   "2775"
+  action :create
+end

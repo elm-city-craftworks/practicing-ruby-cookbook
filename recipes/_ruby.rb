9fec00b7bb9e9a0de522269fc491cc4dbde26eed
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-21 11:26:50 +0200

    Update to the latest RubyGems version

--- recipes/_ruby.rb
+++ recipes/_ruby.rb
@@ -15,6 +15,12 @@ node.set["chruby"]["default"] = node["practicingruby"]["ruby"]["version"]
 # Build and install Ruby versions using chruby und ruby-build
 include_recipe "chruby::system"
 
+# Update to the latest RubyGems version
+execute "update-rubygems" do
+  command "#{node["practicingruby"]["ruby"]["gem"]["binary"]} update --system"
+  action :run
+end
+
 # Install Bundler
 gem_package "bundler" do
   gem_binary node["practicingruby"]["ruby"]["gem"]["binary"]

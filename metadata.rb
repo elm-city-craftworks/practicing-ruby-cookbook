674973d9b6ea91b9ebbac864c1a99361bb457873
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-22 16:55:19 +0200

    Add production recipe to make it clear *what* we are deploying
    This will be included by the default recipe, so no change in behaviour.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license           "Apache 2.0"
 description       "Sets up environment for Practicing Ruby Rails app"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
-recipe            "practicingruby::default", "Sets up environment for Practicing Ruby Rails app"
+recipe            "practicingruby::default", "Includes production recipe"
+recipe            "practicingruby::production", "Sets up production environment for Practicing Ruby Rails app"
 
 supports "ubuntu", ">= 12.04"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,16 +2,7 @@
 # Cookbook Name:: practicingruby
 # Recipe:: default
 #
-# Sets up environment for Practicing Ruby Rails app
+# Includes production recipe
 #
 
-# Update Debian package index
-include_recipe "apt::default"
-
-# Include all the pieces
-include_recipe "practicingruby::_deploy_user"
-include_recipe "practicingruby::_ruby"
-include_recipe "practicingruby::_postgresql"
-include_recipe "practicingruby::_nginx"
-include_recipe "practicingruby::_god"
-include_recipe "practicingruby::_rails"
+include_recipe "practicingruby::production"

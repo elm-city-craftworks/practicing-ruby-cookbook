da3ff0fbce2275f28665922dbab03375cfbcc6ad
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-23 18:03:41 +0200

    1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
-1.0.0 (Oct 22 2012)
+1.0.1 (Oct 23 2013)
+-------------------
+
+* Add attribute `node["practicingruby"]["rails"]["host"]` allowing us to set
+  Rails host name and Nginx server name to, e.g., `practicingruby.local`.
+* Load node attributes and run list from JSON file `chef.log`.
+* Fix: Reload Nginx when SSL files have changed.
+
+1.0.0 (Oct 22 2013)
 -------------------
 
 * Initial public release.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mathias.lafeldt@gmail.com"
 license           "Apache 2.0"
 description       "Sets up environment for Practicing Ruby Rails app"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
 recipe            "practicingruby::default", "Includes production recipe"
 recipe            "practicingruby::production", "Sets up production environment for Practicing Ruby Rails app"
 

9b31b3e25d9583945f49e4383c89b107adc01de6
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-30 17:00:59 +0100

    Remove Chef attributes for gem_package

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,7 @@
 #
 
 # Ruby version to install
-default["practicingruby"]["ruby"]["version"]        = "2.0.0-p247"
-
-# Defaults for gem_package resource
-default["practicingruby"]["ruby"]["gem"]["binary"]  = "/opt/rubies/#{node["practicingruby"]["ruby"]["version"]}/bin/gem"
-default["practicingruby"]["ruby"]["gem"]["options"] = "--no-ri --no-rdoc"
+default["practicingruby"]["ruby"]["version"] = "2.0.0-p247"
 
 # Databases to create
 

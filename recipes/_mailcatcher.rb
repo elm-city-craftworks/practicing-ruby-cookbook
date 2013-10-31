b3cfe56c6ee18fe3662bf2b4800e69ccad4b9713
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-10-31 11:25:51 +0100

    Add mailcatcher recipe

---
+++ recipes/_mailcatcher.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: practicingruby
+# Recipe:: mailcatcher
+#
+# Installs and starts MailCatcher
+#
+
+# Install Ruby first
+include_recipe "practicingruby::_ruby"
+
+# Install MailCatcher
+include_recipe "mailcatcher::default"
+
+# Start MailCatcher as mailcatcher::default doesn't do it for us
+execute "start-mailcatcher" do
+  command  "true"
+  action   :run
+  notifies :start, "service[mailcatcher]"
+  not_if   "pgrep mailcatcher"
+end

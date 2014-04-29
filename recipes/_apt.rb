602ba7a71d647b97cb8c0f7e9fe0d6913ac7a255
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-04-29 15:21:55 +0200

    Work around bug in postgresql cookbook
    Addresses https://github.com/elm-city-craftworks/practicing-ruby-cookbook/issues/24

--- recipes/_apt.rb
+++ recipes/_apt.rb
@@ -6,3 +6,11 @@
 #
 
 include_recipe "apt::default"
+
+# XXX: work around bug in postgresql cookbook
+# https://github.com/elm-city-craftworks/practicing-ruby-cookbook/issues/24
+stamp_file = "/var/lib/apt/periodic/update-success-stamp"
+execute "apt-get update" do
+  action :nothing
+end.run_action(:run) unless ::File.exists?(stamp_file) &&
+                            ::File.mtime(stamp_file) < Time.now - 86_400

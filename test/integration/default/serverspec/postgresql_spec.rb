0a7ceb91949a054592b20f5ccfee7a88323121d6
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-05-08 21:09:27 +0200

    Tweak postgresql test

--- test/integration/default/serverspec/postgresql_spec.rb
+++ test/integration/default/serverspec/postgresql_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe "practicingruby::_postgresql" do
   let(:postgresql_version) { "9.1" }
+  let(:postgresql_port) { 5432 }
 
   it "installs PostgreSQL server" do
     expect(package "postgresql-#{postgresql_version}").to be_installed
@@ -12,14 +13,13 @@ describe "practicingruby::_postgresql" do
   end
 
   it "starts PostgreSQL server" do
-    output = /^Running clusters: #{postgresql_version}\/main/
+    output = "Running clusters: #{postgresql_version}/main"
     expect(command "service postgresql status").to return_stdout output
-
-    expect(port 5432).to be_listening
+    expect(port postgresql_port).to be_listening
   end
 
   it "creates database for Rails app" do
     db_name = "practicing-ruby-production"
-    expect(command "sudo -u postgres psql -l").to return_stdout(/#{db_name}/)
+    expect(command "sudo -u postgres psql -l #{db_name}").to return_exit_status 0
   end
 end

0f0a34ee85d3914f4c0711d04fceeef90d3911e9
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-05-08 23:02:17 +0200

    Actually run `apt-get update` if required
    Fixes the fix in 602ba7a71d647b97cb8c0f7e9fe0d6913ac7a255

--- recipes/_apt.rb
+++ recipes/_apt.rb
@@ -7,10 +7,17 @@
 
 include_recipe "apt::default"
 
+#
 # XXX: work around bug in postgresql cookbook
 # https://github.com/elm-city-craftworks/practicing-ruby-cookbook/issues/24
-stamp_file = "/var/lib/apt/periodic/update-success-stamp"
+#
+def apt_update?
+  stamp_file = "/var/lib/apt/periodic/update-success-stamp"
+  return true unless ::File.exist?(stamp_file)
+  return true if ::File.exist?(stamp_file) && ::File.mtime(stamp_file) < Time.now - 86_400
+  false
+end
+
 execute "apt-get update" do
   action :nothing
-end.run_action(:run) unless ::File.exist?(stamp_file) &&
-                            ::File.mtime(stamp_file) < Time.now - 86_400
+end.run_action(:run) if apt_update?

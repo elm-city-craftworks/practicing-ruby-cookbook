f8fd6ea7b3bb12cfade3f9a6a058d2c8fa4710c3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2015-01-08 21:38:36 +0100

    Update Serverspec spec helper

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,10 +1,4 @@
 require "serverspec"
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = "/sbin:/usr/sbin"
-  end
-end
+set :backend, :exec
+set :path, "/sbin:/usr/sbin:$PATH"
